3bc942fb6de3811db28552753afa1d5775e91148
Jani <janimarton22@gmail.com>
2018-10-24 17:23:07 +0200

    skeleton codes and files added

--- src/com/codecool/Race.java
+++ src/com/codecool/Race.java
@@ -2,7 +2,15 @@ package com.codecool;
 
 public class Race {
 
+//    createVehicles() // creates 10 cars, 10 trucks and 10 motorcycles.
+//    simulateRace() // simulates the race by
+//    // - calling moveForAnHour() on every vehicle 50 times
+//    // - setting whether its raining
+//    printRaceResults() // prints each vehicle's name, distance traveled ant type.
+//    boolean isThereABrokenTruck() //
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello program");
+
     }
 }
