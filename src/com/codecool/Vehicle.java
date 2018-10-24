7bdb5cf4085cf88ac8eb8b6fd90505e523efba57
Jani <janimarton22@gmail.com>
2018-10-24 21:42:28 +0200

    class names are beginning with large words now

--- src/com/codecool/Vehicle.java
+++ src/com/codecool/Vehicle.java
@@ -4,7 +4,7 @@ public class Vehicle {
 
 }
 
-class car extends Vehicle {
+class Car extends Vehicle {
 
 //    ormalSpeed // the normal speed of the car. Set to a random number in the constructor between 80-110km/h.
 //            name // Make a list from the words here: http://www.fantasynamegenerators.com/car-names.php
@@ -16,7 +16,7 @@ class car extends Vehicle {
 
 }
 
-class truck extends Vehicle {
+class Truck extends Vehicle {
 //    Truck // speed: 100km/h. 5% chance of breaking down for 2 hours.
 //            // Truck drivers are boring. They call all their trucks a random number between 0 and 1000.
 //            breakdownTurnsLeft // holds how long its still broken down.
@@ -25,7 +25,7 @@ class truck extends Vehicle {
 
 }
 
-class motorcycles extends Vehicle {
+class Motorcycles extends Vehicle {
 //    Motorcycle // speed is 100km/h. If it rains, travels with 5-50km/h slower (randomly).
 //    static nameNumber // The number of the instance created. Used for its name.
 //            name // Are called "Motorcycle 1", "Motorcycle 2", "Motorcycle 3",... Unique.
