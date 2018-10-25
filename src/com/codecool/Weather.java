7dfa7b86239dba6b698c92651c447d954a332639
Jani <janimarton22@gmail.com>
2018-10-25 11:51:52 +0200

    setRaining() and isRaining() is done

--- src/com/codecool/Weather.java
+++ src/com/codecool/Weather.java
@@ -1,8 +1,26 @@
 package com.codecool;
 
+import java.util.*;
+
 public class Weather {
 
 //    static setRaining() // 30% chance of rain.
 //    static boolean isRaining() // is it raining currently.
+
+    private static boolean setRaining(){
+        boolean itIsRaining = false;
+        Random allCases = new Random(9);
+        int chance = allCases.nextInt();
+        if (chance <= 2){
+            itIsRaining = true;
+        }
+        return itIsRaining;
+    }
+
+    static boolean isRaining(){
+        boolean currentlyRaining = setRaining();
+        return currentlyRaining;
+    }
+
 
 }
