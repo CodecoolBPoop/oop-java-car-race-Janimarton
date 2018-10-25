9f43e2876e43a1927baaa3eedc6f8e2d36d233a2
Jani <janimarton22@gmail.com>
2018-10-25 14:57:19 +0200

    in the vehicle file the car class is done

--- src/com/codecool/Vehicle.java
+++ src/com/codecool/Vehicle.java
@@ -1,12 +1,17 @@
 package com.codecool;
 
-public class Vehicle {
+import java.util.Random;
+
+abstract class Vehicle {
+    String name;
+    int normalSpeed;
+    int distanceTraveled;
 
 }
 
 class Car extends Vehicle {
 
-//    ormalSpeed // the normal speed of the car. Set to a random number in the constructor between 80-110km/h.
+//    normalSpeed // the normal speed of the car. Set to a random number in the constructor between 80-110km/h.
 //            name // Make a list from the words here: http://www.fantasynamegenerators.com/car-names.php
 // and pick 2 randomly for each instance.
 //    distanceTraveled // holds the current distance traveled.
@@ -14,6 +19,37 @@ class Car extends Vehicle {
 // Call this from the main class only!
 //    // If there is a broken down Truck on the track, then limit the max speed of vehicles to 75 km/h.
 
+    public Car() {
+        this.name = getCarName();
+        this.normalSpeed = getNormalSpeed();
+        this.distanceTraveled = 0;
+
+
+    }
+
+    private String[] cars = {"Expedition", "Purity", "Eminance", "Supremacy", "Blast", "Specter", "Eon", "Capital",
+            "Empyrean", "Revelation"};
+
+    private String getCarName() {
+        String carNames = cars[new Random().nextInt(cars.length)];
+        carNames = carNames + " " + cars[new Random().nextInt(cars.length)];
+        return carNames;
+    }
+
+    private int getNormalSpeed() {
+        int normalSpeed = (int) (Math.random() * 30) + 80;
+        return normalSpeed;
+    }
+
+    public void setNormalSpeedToMax() {
+        int maxSpeed = 75;
+        this.normalSpeed = maxSpeed;
+    }
+
+    public void moveForAnHour() {
+        int speedForAnHour = this.normalSpeed;
+        this.distanceTraveled += speedForAnHour;
+    }
 }
 
 class Truck extends Vehicle {
