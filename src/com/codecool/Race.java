1b4921eaf3fd6128c3575e4bb1c1c3ad8badf102
Jani <janimarton22@gmail.com>
2018-10-25 16:11:32 +0200

    in the race file the car and the truck are tested

--- src/com/codecool/Race.java
+++ src/com/codecool/Race.java
@@ -1,5 +1,8 @@
 package com.codecool;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Race {
 
 //    createVehicles() // creates 10 cars, 10 trucks and 10 motorcycles.
@@ -10,7 +13,29 @@ public class Race {
 //    boolean isThereABrokenTruck() //
 
     public static void main(String[] args) {
-        System.out.println("Hello program");
+        System.out.println("The race begins \n");
+        System.out.println("The cars: \n");
+        Car [] cars = new Car[10];
+        for (int i=0; i<cars.length; i++){
+            cars[i] = new Car();
+        }
+        for (Car car : cars) {
+            System.out.println(car.name + " is goes " + car.normalSpeed + " km/h to: " + car.distanceTraveled);
+        }
+        cars[0].moveForAnHour();
+        System.out.println(cars[0].distanceTraveled +"\n");
+        System.out.println("The trucks: \n");
+        Truck [] trucks = new Truck[10];
+        for (int i = 0; i < trucks.length; i++) {
+            trucks[i] = new Truck();
+        }
+        for (Truck truck:
+             trucks) {
+            System.out.println(truck.name + " is goes " + truck.normalSpeed + " km/h to: " + truck.distanceTraveled);
+
+        }
+        trucks[0].moveForAnHour();
+        System.out.println(trucks[0].distanceTraveled);
 
     }
 }
